d9b4d3326180b19851af4838f737a33fcc07f9dc
trzl <brownty47@gmail.com>
2018-08-14 17:22:44 -0500

    First working version
    * fixed bug where score increased by two
     - projectile shared "Collision" tag which added an additional point
     from DestroyTransform
     - tagged Projectile as "Projectile" to let DestroyTransform still work
     without additional point
    
    * added ScoreLogic
     - shooting Enemy sprites increases score by 1

--- Assets/Scripts/DestroyTransform.cs
+++ Assets/Scripts/DestroyTransform.cs
@@ -7,7 +7,11 @@ public class DestroyTransform : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D transformCollision) {
 		if (transformCollision.gameObject.tag == "Collision") {
 			Destroy (transformCollision.gameObject);
+			GameObject.FindGameObjectWithTag("GUI").GetComponent<ScoreLogic>().addToScoreVOID();
 		}
 
+		if (transformCollision.gameObject.tag == "Projectile") {
+			Destroy (transformCollision.gameObject);
+		}
 	}
 }

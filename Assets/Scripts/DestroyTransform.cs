e5b4610df0a1e6b7b0c6d320966a41669c3d9c3c
trzl <brownty47@gmail.com>
2018-08-14 23:53:13 -0500

    Added in explosion when Enemy is destroyed

--- Assets/Scripts/DestroyTransform.cs
+++ Assets/Scripts/DestroyTransform.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class DestroyTransform : MonoBehaviour {
 
+	public GameObject explosion;
 	void OnCollisionEnter2D(Collision2D transformCollision) {
+		spawnExplosion(transformCollision.transform.position);
+
 		if (transformCollision.gameObject.tag == "Collision") {
 			Destroy (transformCollision.gameObject);
 			GameObject.FindGameObjectWithTag("GUI").GetComponent<ScoreLogic>().addToScoreVOID();
@@ -13,5 +16,9 @@ public class DestroyTransform : MonoBehaviour {
 		if (transformCollision.gameObject.tag == "Projectile") {
 			Destroy (transformCollision.gameObject);
 		}
+	}
+
+	void spawnExplosion(Vector2 tempPosition) {
+		Instantiate(explosion, tempPosition, Quaternion.identity);
 	}
 }
